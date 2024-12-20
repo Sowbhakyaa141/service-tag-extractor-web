32497c88b82cca10bccf9f3dd9f8f93a04389bfd
Sowbhakyaa <ssowbhakyaa@gmail.com>
2024-12-20 17:44:23 +0530

    Add deployment configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-// src/App.js
-import React from 'react';
 import ServiceTagExtractor from './components/ServiceTagExtractor';
 
 function App() {
--- src/components/ui/alert.js
+++ src/components/ui/alert.js
@@ -1,13 +1,19 @@
+// src/components/ui/alert.js
+
 import React from 'react';
 
-export const Alert = ({ children, className }) => (
-  <div className={`p-4 border rounded ${className}`}>
-    {children}
-  </div>
-);
+export const Alert = ({ children, className }) => {
+  return (
+    <div className={`alert ${className}`}>
+      {children}
+    </div>
+  );
+};
 
-export const AlertDescription = ({ children, className }) => (
-  <p className={`text-sm ${className}`}>
-    {children}
-  </p>
-);
+export const AlertDescription = ({ children, className }) => {
+  return (
+    <div className={`alert-description ${className}`}>
+      {children}
+    </div>
+  );
+};
